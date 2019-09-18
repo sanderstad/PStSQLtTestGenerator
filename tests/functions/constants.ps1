892facc92656b0e00a49b0771da8599757dd157f
sqlstad <sanderstad@gmail.com>
2019-09-18 15:46:36 +0200

    Renamed file

---
+++ tests/functions/constants.ps1
@@ -0,0 +1,19 @@
+# constants
+if (Test-Path C:\temp\constants.ps1) {
+    Write-Verbose "C:\temp\constants.ps1 found."
+    . C:\temp\constants.ps1
+}
+elseif (Test-Path "$PSScriptRoot\constants.local.ps1") {
+    Write-Verbose "tests\constants.local.ps1 found."
+    . "$PSScriptRoot\constants.local.ps1"
+}
+else {
+    $script:computer = "localhost"
+    $script:instance = "localhost"
+    $script:tempfolder = "C:\temp\"
+    $script:unittestfolder = (Join-Path -Path $script:tempfolder -ChildPath "unittests")
+}
+
+if ($env:appveyor) {
+    $PSDefaultParameterValues['*:WarningAction'] = 'SilentlyContinue'
+}
