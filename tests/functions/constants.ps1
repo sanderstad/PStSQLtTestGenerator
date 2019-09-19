2f9409f68061887165e419c65b8e268f4b572d61
sqlstad <sanderstad@gmail.com>
2019-09-19 09:57:45 +0200

    Removed unused code

--- tests/functions/constants.ps1
+++ tests/functions/constants.ps1
@@ -3,10 +3,6 @@ if (Test-Path C:\temp\constants.ps1) {
     Write-Verbose "C:\temp\constants.ps1 found."
     . C:\temp\constants.ps1
 }
-elseif (Test-Path "$PSScriptRoot\constants.local.ps1") {
-    Write-Verbose "tests\constants.local.ps1 found."
-    . "$PSScriptRoot\constants.local.ps1"
-}
 else {
     $script:computer = "localhost"
     $script:instance = "localhost"
