9d8143320da9a2e707d22a15189cbd1f18ac569b
sqlstad <sanderstad@gmail.com>
2019-09-19 16:56:34 +0200

    Fixed message level

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -2,11 +2,11 @@ Add-AppveyorTest -Name "appveyor.prerequisites" -Framework NUnit -FileName "appv
 $sw = [system.diagnostics.stopwatch]::startNew()
 
 Install-Module PSFramework -Force -SkipPublisherCheck
-Write-PSFMessage -Level Info -Message "Installing dbatools" -ForegroundColor Cyan
+Write-PSFMessage -Level Host -Message "Installing dbatools" -ForegroundColor Cyan
 Install-Module dbatools -Force -SkipPublisherCheck
-Write-PSFMessage -Level Info -Message "Installing Pester" -ForegroundColor Cyan
+Write-PSFMessage -Level Host -Message "Installing Pester" -ForegroundColor Cyan
 Install-Module Pester -Force -SkipPublisherCheck
-Write-PSFMessage -Level Info -Message "Installing PSScriptAnalyzer" -ForegroundColor Cyan
+Write-PSFMessage -Level Host -Message "Installing PSScriptAnalyzer" -ForegroundColor Cyan
 Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck
 
 . "$PSScriptRoot\appveyor-constants.ps1"
