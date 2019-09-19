650f10afe69047b0a596c02338a8c853fca45faf
sqlstad <sanderstad@gmail.com>
2019-09-19 16:43:01 +0200

    Changed Write-Host to Write-PSFMessage

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -1,13 +1,13 @@
 Add-AppveyorTest -Name "appveyor.prerequisites" -Framework NUnit -FileName "appveyor-prerequisites.ps1" -Outcome Running
 $sw = [system.diagnostics.stopwatch]::startNew()
 
-Write-Host "Installing dbatools" -ForegroundColor Cyan
+Write-PSFMessage -Level Info -Message "Installing dbatools" -ForegroundColor Cyan
 Install-Module dbatools -Force -SkipPublisherCheck
-Write-Host "Installing Pester" -ForegroundColor Cyan
+Write-PSFMessage -Level Info -Message "Installing Pester" -ForegroundColor Cyan
 Install-Module Pester -Force -SkipPublisherCheck
-Write-Host "Installing PSFramework" -ForegroundColor Cyan
+Write-PSFMessage -Level Info -Message "Installing PSFramework" -ForegroundColor Cyan
 Install-Module PSFramework -Force -SkipPublisherCheck
-Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
+Write-PSFMessage -Level Info -Message "Installing PSScriptAnalyzer" -ForegroundColor Cyan
 Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck
 
 . "$PSScriptRoot\appveyor-constants.ps1"
