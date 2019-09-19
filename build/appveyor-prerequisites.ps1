aaf8b625c8cb75aceec111a42c140dbf93e91669
sqlstad <sanderstad@gmail.com>
2019-09-19 16:52:34 +0200

    Moved module install Removed info message

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -1,12 +1,11 @@
 Add-AppveyorTest -Name "appveyor.prerequisites" -Framework NUnit -FileName "appveyor-prerequisites.ps1" -Outcome Running
 $sw = [system.diagnostics.stopwatch]::startNew()
 
+Install-Module PSFramework -Force -SkipPublisherCheck
 Write-PSFMessage -Level Info -Message "Installing dbatools" -ForegroundColor Cyan
 Install-Module dbatools -Force -SkipPublisherCheck
 Write-PSFMessage -Level Info -Message "Installing Pester" -ForegroundColor Cyan
 Install-Module Pester -Force -SkipPublisherCheck
-Write-PSFMessage -Level Info -Message "Installing PSFramework" -ForegroundColor Cyan
-Install-Module PSFramework -Force -SkipPublisherCheck
 Write-PSFMessage -Level Info -Message "Installing PSScriptAnalyzer" -ForegroundColor Cyan
 Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck
 
