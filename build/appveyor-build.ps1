9524de28aa36754e5ac4a22e92d8e919d9748ccd
sqlstad <sanderstad@gmail.com>
2019-09-21 22:12:43 +0200

    Changed build script

--- build/appveyor-build.ps1
+++ build/appveyor-build.ps1
@@ -5,29 +5,41 @@ It expects as input an ApiKey authorized to publish the module.
 Insert any build steps you may need to take before publishing it here.
 #>
 param (
-    $ApiKey
+    $ApiKey,
+
+    $WorkingDirectory,
+
+    $Repository = 'PSGallery',
+
+    [switch]
+    $LocalRepo,
+
+    [switch]
+    $SkipPublish,
+
+    [switch]
+    $AutoVersion
 )
+
+#region Handle Working Directory Defaults
+if (-not $WorkingDirectory) {
+    if ($env:RELEASE_PRIMARYARTIFACTSOURCEALIAS) {
+        $WorkingDirectory = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath $env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
+    }
+    else { $WorkingDirectory = $env:SYSTEM_DEFAULTWORKINGDIRECTORY }
+}
+#endregion Handle Working Directory Defaults
 
 # Prepare publish folder
 Write-PSFMessage -Level Important -Message "Creating and populating publishing directory"
+$publishDir = New-Item -Path $WorkingDirectory -Name publish -ItemType Directory
+Copy-Item -Path "$($WorkingDirectory)\PStSQLtTestGenerator" -Destination $publishDir.FullName -Recurse -Force
 
-$publishDir = New-Item -Path "C:\Projects\" -Name publish -ItemType Directory
-
-Copy-Item -Path "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\PStSQLtTestGenerator" -Destination $publishDir.FullName -Recurse -Force
-
-# Create commands.ps1
+#region Gather text data to compile
 $text = @()
-Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
-    $text += [System.IO.File]::ReadAllText($_.FullName)
-}
-Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
-    $text += [System.IO.File]::ReadAllText($_.FullName)
-}
-$text -join "`n`n" | Set-Content -Path "$($publishDir.FullName)\PStSQLtTestGenerator\commands.ps1"
-
-# Create resourcesBefore.ps1
 $processed = @()
-$text = @()
+
+# Gather Stuff to run before
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object { $_ -notlike "#*" })) {
     if ([string]::IsNullOrWhiteSpace($line)) { continue }
 
@@ -40,11 +52,16 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object
         $processed += $item.FullName
     }
 }
-if ($text) { $text -join "`n`n" | Set-Content -Path "$($publishDir.FullName)\PStSQLtTestGenerator\resourcesBefore.ps1" }
 
-# Create resourcesAfter.ps1
-$processed = @()
-$text = @()
+# Gather commands
+Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
+    $text += [System.IO.File]::ReadAllText($_.FullName)
+}
+Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
+    $text += [System.IO.File]::ReadAllText($_.FullName)
+}
+
+# Gather stuff to run afterwards
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object { $_ -notlike "#*" })) {
     if ([string]::IsNullOrWhiteSpace($line)) { continue }
 
@@ -57,7 +74,43 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object 
         $processed += $item.FullName
     }
 }
-if ($text) { $text -join "`n`n" | Set-Content -Path "$($publishDir.FullName)\PStSQLtTestGenerator\resourcesAfter.ps1" }
+#endregion Gather text data to compile
 
-# Publish to Gallery
-Publish-Module -Path "$($publishDir.FullName)\PStSQLtTestGenerator" -NuGetApiKey $ApiKey -Force #>
+#region Update the psm1 file
+$fileData = Get-Content -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psm1" -Raw
+$fileData = $fileData.Replace('"<was not compiled>"', '"<was compiled>"')
+$fileData = $fileData.Replace('"<compile code into here>"', ($text -join "`n`n"))
+[System.IO.File]::WriteAllText("$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psm1", $fileData, [System.Text.Encoding]::UTF8)
+#endregion Update the psm1 file
+
+#region Updating the Module Version
+if ($AutoVersion) {
+    Write-PSFMessage -Level Important -Message "Updating module version numbers."
+    try { [version]$remoteVersion = (Find-Module 'PStSQLtTestGenerator' -Repository $Repository -ErrorAction Stop).Version }
+    catch {
+        Stop-PSFFunction -Message "Failed to access $($Repository)" -EnableException $true -ErrorRecord $_
+    }
+    if (-not $remoteVersion) {
+        Stop-PSFFunction -Message "Couldn't find PStSQLtTestGenerator on repository $($Repository)" -EnableException $true
+    }
+    $newBuildNumber = $remoteVersion.Build + 1
+    [version]$localVersion = (Import-PowerShellDataFile -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psd1").ModuleVersion
+    Update-ModuleManifest -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psd1" -ModuleVersion "$($localVersion.Major).$($localVersion.Minor).$($newBuildNumber)"
+}
+#endregion Updating the Module Version
+
+#region Publish
+if ($SkipPublish) { return }
+if ($LocalRepo) {
+    # Dependencies must go first
+    Write-PSFMessage -Level Important -Message "Creating Nuget Package for module: PSFramework"
+    New-PSMDModuleNugetPackage -ModulePath (Get-Module -Name PSFramework).ModuleBase -PackagePath .
+    Write-PSFMessage -Level Important -Message "Creating Nuget Package for module: PStSQLtTestGenerator"
+    New-PSMDModuleNugetPackage -ModulePath "$($publishDir.FullName)\PStSQLtTestGenerator" -PackagePath .
+}
+else {
+    # Publish to Gallery
+    Write-PSFMessage -Level Important -Message "Publishing the PStSQLtTestGenerator module to $($Repository)"
+    Publish-Module -Path "$($publishDir.FullName)\PStSQLtTestGenerator" -NuGetApiKey $ApiKey -Force -Repository $Repository
+}
+#endregion Publish
