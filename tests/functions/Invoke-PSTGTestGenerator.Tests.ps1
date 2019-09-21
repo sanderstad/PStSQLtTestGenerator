7527b05cd8ccf6e96fc7acfea51e11d82195c051
sqlstad <sanderstad@gmail.com>
2019-09-21 15:39:30 +0200

    Fixed test

--- tests/functions/Invoke-PSTGTestGenerator.Tests.ps1
+++ tests/functions/Invoke-PSTGTestGenerator.Tests.ps1
@@ -46,7 +46,7 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
         }
 
         It "Should have all the tests" {
-            $files.Count | Should -Be 12
+            $files.Count | Should -Be 10
         }
 
         It "Result should have correct values" {
