0cfba3fa8b4f346daa2fe1a5132b7408d29bd3fe
sqlstad <sanderstad@gmail.com>
2019-09-22 09:08:06 +0200

    Added check for cached modules

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -1,13 +1,24 @@
 Add-AppveyorTest -Name "appveyor.prerequisites" -Framework NUnit -FileName "appveyor-prerequisites.ps1" -Outcome Running
 $sw = [system.diagnostics.stopwatch]::startNew()
 
-Install-Module PSFramework -Force -SkipPublisherCheck
-Write-PSFMessage -Level Host -Message "Installing dbatools"
-Install-Module dbatools -Force -SkipPublisherCheck
-Write-PSFMessage -Level Host -Message "Installing Pester"
-Install-Module Pester -Force -SkipPublisherCheck
-Write-PSFMessage -Level Host -Message "Installing PSScriptAnalyzer"
-Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck
+if (-not (Get-Module -Name PSFramework)) {
+    Install-Module PSFramework -Force -SkipPublisherCheck
+}
+
+if (-not (Get-Module -Name dbatools)) {
+    Write-PSFMessage -Level Host -Message "Installing dbatools"
+    Install-Module dbatools -Force -SkipPublisherCheck
+}
+
+if (-not (Get-Module -Name Pester)) {
+    Write-PSFMessage -Level Host -Message "Installing Pester"
+    Install-Module Pester -Force -SkipPublisherCheck
+}
+
+if (-not (Get-Module -Name PSScriptAnalyzer)) {
+    Write-PSFMessage -Level Host -Message "Installing PSScriptAnalyzer"
+    Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck
+}
 
 . "$PSScriptRoot\appveyor-constants.ps1"
 
