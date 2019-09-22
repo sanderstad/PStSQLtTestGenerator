9004241103611ed66ed5056fb303c170a23b29e6
sqlstad <sanderstad@gmail.com>
2019-09-22 13:52:32 +0200

    Moved pester install up

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -3,12 +3,20 @@ $sw = [system.diagnostics.stopwatch]::startNew()
 
 $modules = (Get-Module -ListAvailable) | Select-Object Name, Version
 
+if ($modules.Name -notcontains "Pester") {
+    #    Write-PMessage -Level Host -Message "Installing Pester"
+    Install-Module Pester -MinimumVersion "4.8.1" -Force -SkipPublisherCheck
+
+    Import-Module Pester
+    Get-Module -Name Pester
+}
+
 if ($modules.Name -notcontains "PSFramework") {
     Install-Module PSFramework -Force -SkipPublisherCheck
 }
 
 if ($modules.Name -notcontains "Pester") {
-    Write-PSFMessage -Level Host -Message "Installing Pester"
+    #    Write-PMessage -Level Host -Message "Installing Pester"
     Install-Module Pester -MinimumVersion "4.8.1" -Force -SkipPublisherCheck
 
     Import-Module Pester
