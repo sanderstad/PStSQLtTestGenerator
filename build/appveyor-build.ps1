6b7421a82669da42ee7ee2063dbfabb1fdc91b25
sqlstad <sanderstad@gmail.com>
2019-09-22 14:57:09 +0200

    Added removal of yaml file

--- build/appveyor-build.ps1
+++ build/appveyor-build.ps1
@@ -48,6 +48,7 @@ catch {
 
 # region remove unneccesary directories
 try {
+    Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\appveyor.yml" -Force -Recurse
     Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\build" -Force -Recurse
     Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\resources" -Force -Recurse
     Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\tests" -Force -Recurse
