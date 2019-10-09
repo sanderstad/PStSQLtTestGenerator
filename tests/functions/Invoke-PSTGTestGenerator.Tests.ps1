2162404f508dbe52d804785045820dbe557016be
sqlstad <sanderstad@gmail.com>
2019-10-09 15:35:34 +0200

    Added missing parameters

--- tests/functions/Invoke-PSTGTestGenerator.Tests.ps1
+++ tests/functions/Invoke-PSTGTestGenerator.Tests.ps1
@@ -5,7 +5,7 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'OutputPath', 'TemplateFolder', 'Function', 'Procedure', 'Table', 'View', 'SkipDatabaseTests', 'SkipFunctionTests', 'SkipProcedureTests', 'SkipTableTests', 'SkipViewTests', 'TestClass', 'EnableException'
+        [object[]]$knownParameters = 'SqlInstance', 'SqlCredential', 'Database', 'OutputPath', 'TemplateFolder', 'Function', 'Procedure', 'Table', 'Index', 'View', 'SkipDatabaseTests', 'SkipFunctionTests', 'SkipProcedureTests', 'SkipTableTests', 'SkipIndexTests', 'SkipViewTests', 'TestClass', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
@@ -55,9 +55,9 @@ Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
     }
 
     AfterAll {
-        $null = Remove-DbaDatabase -SqlInstance $script:sqlinstance -Database $script:database -Confirm:$false
+        #$null = Remove-DbaDatabase -SqlInstance $script:sqlinstance -Database $script:database -Confirm:$false
 
-        $null = Remove-Item -Path $script:unittestfolder -Recurse -Force -Confirm:$false
+        #$null = Remove-Item -Path $script:unittestfolder -Recurse -Force -Confirm:$false
     }
 
 }
