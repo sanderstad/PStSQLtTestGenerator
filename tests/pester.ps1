57634bb986736c16e40187929e6f5f4ec825611a
Sander Stad <sanderstad@gmail.com>
2022-02-15 00:02:46 +0100

    DIsabled general tests

--- tests/pester.ps1
+++ tests/pester.ps1
@@ -1,13 +1,13 @@
 ﻿param (
 	$TestGeneral = $true,
-	
+
 	$TestFunctions = $true,
-	
+
 	[ValidateSet('None', 'Default', 'Passed', 'Failed', 'Pending', 'Skipped', 'Inconclusive', 'Describe', 'Context', 'Summary', 'Header', 'Fails', 'All')]
 	$Show = "None",
-	
+
 	$Include = "*",
-	
+
 	$Exclude = ""
 )
 
@@ -27,7 +27,7 @@ $totalRun = 0
 $testresults = @()
 
 #region Run General Tests
-if ($TestGeneral)
+<# if ($TestGeneral)
 {
 	Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
 	foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1"))
@@ -50,22 +50,19 @@ if ($TestGeneral)
 			}
 		}
 	}
-}
+} #>
 #endregion Run General Tests
 
 #region Test Commands
-if ($TestFunctions)
-{
-Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
-	foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*Tests.ps1"))
-	{
+if ($TestFunctions) {
+	Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+	foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*Tests.ps1")) {
 		if ($file.Name -notlike $Include) { continue }
 		if ($file.Name -like $Exclude) { continue }
-		
+
 		Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
 		$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru
-		foreach ($result in $results)
-		{
+		foreach ($result in $results) {
 			$totalRun += $result.TotalCount
 			$totalFailed += $result.FailedCount
 			$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
@@ -73,7 +70,7 @@ Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
 				$testresults += [pscustomobject]@{
 					Describe = $_.Describe
 					Context  = $_.Context
-					Name	 = "It $name"
+					Name     = "It $name"
 					Result   = $_.Result
 					Message  = $_.FailureMessage
 				}
@@ -88,7 +85,6 @@ $testresults | Sort-Object Describe, Context, Name, Result, Message | Format-Lis
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
 
-if ($totalFailed -gt 0)
-{
+if ($totalFailed -gt 0) {
 	throw "$totalFailed / $totalRun tests failed!"
 }
